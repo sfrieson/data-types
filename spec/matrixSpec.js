0bf75287b38340b798d02dce5468124b2c4bb8af
Steven Frieson <sfrieson@gmail.com>
2016-05-14 23:06:46 -0400

    Separates matrix.traverse() option parsing

--- spec/matrixSpec.js
+++ spec/matrixSpec.js
@@ -21,13 +21,32 @@ describe("Matrix.traverse(fn[, opt])", function(){
       .toEqual("00,01,02,03,10,11,12,13,20,21,22,23,30,31,32,33,");
     });
   });
-  describe("when called with options", function(){
+  describe("when called with specific row and column options", function(){
     it("should follow options and replace missing with defaults", function(){
       var result = "";
       mat.traverse(function(_item, y, x){result += "" + y + x + ",";},
       {row:{start:1, end:0, increment:-1}});
       expect(result)
       .toEqual("01,00,11,10,21,20,31,30,");
+    });
+  });
+  describe("when called with specific general options", function(){
+    it("should use options for both row and column inserting defaults", function(){
+      var result = "";
+      mat.traverse(function(_item, y, x){result += "" + y + x + ",";},
+      {start:1, end:0, increment:-1});
+      expect(result)
+      .toEqual("11,10,01,00,");
+      result = "";
+    });
+  });
+  describe("when called with negative increment and no start", function(){
+    it("should start from the traverse from bottom right to top left", function(){
+      var result = "";
+      mat.traverse(function(_item, y, x){result += "" + y + x + ",";},
+      {increment:-1});
+      expect(result)
+      .toEqual("33,32,31,30,23,22,21,20,13,12,11,10,03,02,01,00,");
     });
   });
 });
--- src/matrix.js
+++ src/matrix.js
@@ -13,67 +13,14 @@ var matrix = function(width, height){
 matrix.prototype = Object.create(Array.prototype);
 
 matrix.prototype.traverse = function(fn, options){
-  var x, y, cell,
-  opt = {},
-  defaultOpt = {
-    col:{
-      start: 0,
-      end: this.length,
-      inc: 1,
-      comp: "<"
-    },
-    row:{
-      start: 0,
-      //end: built into the loop for variable lengths,
-      inc: 1,
-      comp: "<"
-    }
-  };
-  //No options? Take all defaults
-  if(!options) opt = defaultOpt;
-  //Partial or complete options
-  else {
-    if(options.column){
-      opt.col = {
-        start: options.column.start || 0,
-        inc: options.column.increment || 1,
-        comp: options.column.comparison || null, //default set below
-        end: typeof options.column.end == "number" ? options.column.end : null //defaults set later
-      };
-
-      //comparison is not yet set
-      if(!opt.col.comp) {
-        //and we're decrementing
-        opt.col.comp = opt.col.inc < 0 ? '>=' : '<';
-        opt.col.end = opt.col.inc < 0 ? (options.col.end || 0) : (opt.col.end || this.length);
-      }
-
-    } else opt.col = defaultOpt.col;
-
-
-    if(options.row) {
-      opt.row = {
-        start: options.row.start || 0,
-        inc: options.row.increment || 1,
-        comp: options.row.comparison || null, //default set below
-        end: typeof options.row.end == "number" ? options.row.end : null //default set in loop
-      };
-
-      //comparison is not yet set
-      if(!opt.row.comp) {
-
-        //and we're decrementing
-        opt.row.comp = opt.row.inc < 0 ? '>=' : '<';
-        if(opt.row.inc < 0 ) opt.row.end = options.row.end || 0;
-      }
-    } else opt.row = defaultOpt.row;
-  }
-
+  var opt = parseOptions(options, this.length);
+  // console.log(opt);
   // Looping
   // console.log("col:",opt.col);
   loop[opt.col.comp](opt.col.start, opt.col.end, opt.col.inc,
     function(y){//body of outer
-      if(typeof opt.row.end !== "number") opt.row.end = this[y].length;
+      //get extra row options
+      addRowOptions(opt.row, this[y]);
       // console.log("row:",opt.row);
       loop[opt.row.comp](opt.row.start, opt.row.end, opt.row.inc,
         function(x){fn(this[y][x], y, x, this);}.bind(this)); //body of inner
@@ -95,10 +42,102 @@ matrix.prototype.column = function(index){
   var col = [];
 
   this.traverse(function(item){col.push(item);},
-  {row:{start:index, end: index+1, increment: 1}});
+  {row:{start:index, end: index + 1, increment: 1}});
   return col;
 };
 module.exports = matrix;
+
+function parseOptions (opts, matLength){
+  var row = {}, col = {},
+      defaultOpt = {
+        col:{start: 0, end: matLength, inc: 1, comp: "<"},
+        row:{start: 0, inc: 1, comp: "<"} //end: in the loop for variable lengths,
+      };
+
+  //No opts? Use all defaults
+  if(!opts) return defaultOpt;
+
+  //using specific options
+  else if(opts.column || opts.row){
+    if(opts.column){
+      //change defaults if negative increment is found
+      if(opts.column.increment && opts.column.increment < 0){
+        defaultOpt.col.start = matLength - 1;
+        defaultOpt.col.end = 0;
+      }
+      col = {
+        start: opts.column.start || defaultOpt.col.start,
+        inc: opts.column.increment || defaultOpt.col.inc,
+        comp: opts.column.comparison || null, //default set below
+        end: typeof opts.column.end == "number" ? opts.column.end : null //defaults set later
+      };
+
+      //comparison is not yet set
+      if(!col.comp) {
+        //decrementing or incrementing
+        col.comp = col.inc < 0 ? '>=' : defaultOpt.col.comp;
+      }
+
+      //end is not yet set
+      if(typeof col.end !== "number"){
+        //decrementing or incrementing
+        col.end = col.inc < 0 ? (opts.column.end || 0) : (col.end || defaultOpt.col.end);
+      }
+
+    } else col = defaultOpt.col;
+
+
+    if(opts.row) {
+      //change defaults if negative increment is found
+      if(opts.row.increment && opts.row.increment < 0){
+        defaultOpt.row.start = null; //set in loop for variable lengths
+        defaultOpt.row.end = 0;
+      }
+      row = {
+        start: opts.row.start || defaultOpt.row.start,
+        inc: opts.row.increment || defaultOpt.row.inc,
+        comp: opts.row.comparison || null, //default set below
+        end: typeof opts.row.end == "number" ? opts.row.end : null //default set in loop
+      };
+
+      //comparison is not yet set
+      if(!row.comp) {
+        //and we're decrementing
+        row.comp = row.inc < 0 ? '>=' : defaultOpt.row.comp;
+        if(row.inc < 0 ) row.end = opts.row.end || 0;
+      }
+      if(!row.end) {
+        //set the default end, but still may need to be set in the loop
+        row.end = defaultOpt.row.end || null;
+      }
+    } else row = defaultOpt.row;
+
+    //if using generic options
+  } else {
+    //both will be the same
+    row = col = {
+      start: opts.start,
+      end: opts.end,
+      increment: opts.increment,
+      comparison: opts.comparison
+    };
+    //recall this function with specific version
+    return parseOptions({row: row, column: col}, matLength);
+  }
+
+  return {row:row, col:col};
+}
+
+function addRowOptions(opt, row){
+  //decrementing default
+  if(opt.inc < 0){
+    if(typeof opt.start !== "number") opt.start = row.length - 1;
+    if(typeof opt.end !== "number") opt.end = 0;
+  }
+  //incrementing default
+  if(typeof opt.end !== "number")opt.end = row.length;
+}
+
 
 var loop = {
   ">": function(start, end, increment, body){
